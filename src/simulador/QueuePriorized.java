0cd0ef0de9d3730fe761e71e543e44b5edb75a2c
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-06-11 22:10:03 -0300

    Logica de ordenacao da fila

--- src/simulador/QueuePriorized.java
+++ src/simulador/QueuePriorized.java
@@ -1,10 +1,22 @@
 package simulador;
 
-public class QueuePriorized<Priorizavel> extends QueueLinked<Priorizavel> {
+public class QueuePriorized<E extends Priorizavel> extends QueueLinked<E> {
 
 	@Override
-	public void add(Priorizavel element) {
-		
+	public void add(E element) {
+		Node<E> adicionado = new Node<E>(element);
+		if(element.isPrioritario()) {
+			Node<E> proximo = head;
+			Node<E> anterior = head;
+			while(!proximo.element.isPrioritario()) {
+				anterior = proximo;
+				proximo = proximo.next;
+			}
+			anterior.next = adicionado;
+			adicionado.next = proximo;
+		} else {
+			super.add(element);
+		}
 	}
 	
 }
